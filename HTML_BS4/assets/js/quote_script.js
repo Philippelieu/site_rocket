7585162ce18345d7668b79789953b2aff7aa76b6
Philippe <philippelieu@me.com>
2020-02-05 12:12:45 -0500

    vs 2

--- HTML_BS4/assets/js/quote_script.js
+++ HTML_BS4/assets/js/quote_script.js
@@ -1,4 +1,63 @@
 $(document).ready(fonction(){
 
+    <script> 	
+				
+    function Call()  {
+           FormulesSelected();
+          
+   
+    }</script>
+
+                     <script>
+			
+			
+                     function FormulesSelected()  {
+                     
+                    var service =  MaFonctionDeSelection();  
+                    
+                    
+                    if ( service == 1 )
+                    {
+                       ResultNumberResidentialElevators(); 
+                    }
+                    
+                    if ( service == 2 )
+                    {
+                       ResultNumberCommercialElevators(); 
+                       
+                    }
+                    
+                    if ( service == 3 )
+                    {
+                       ResultNumberCorporateElevators(); 
+                    }
+                    
+                    if ( service == 4 )
+                    {
+                       ResultNumberCorporateElevators(); 
+                    }
+                    
+                    
+                    
+                 }
+                 </script>
+                 
+
+
+    function 
+    if a est selectionné, nous afficherons les var suivante( nbappart,nbetage)
     
-});
+
+    /* arrondir
+    function roundDecimal(nombre, precision){
+    var precision = precision || 2;
+    var tmp = Math.pow(10, precision);
+    return Math.round( nombre*tmp )/tmp;
+    + 1 
+*/
+<script>
+Math.round(3.16)+1
+
+</script>
+
+
