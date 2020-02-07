4e8d76884f6d048e55d74958acb5ea07f0d2c2ff
Philippe <philippelieu@me.com>
2020-02-06 22:57:43 -0500

    maj fin journee

--- HTML_BS4/assets/js/quote.js
+++ HTML_BS4/assets/js/quote.js
@@ -19,6 +19,7 @@ function residential() {
     
 }
 
+
 // Calculation Commercial
 function commercial() {
     console.log("test")
@@ -30,26 +31,50 @@ function commercial() {
     console.log(nbParkingc)
 }
 
-// Calculation Corporate*/
+// Calculation Corporate
 function corporate() {
     console.log("test")
-    nbSeprate_rental = document.getElementById("nbseparte_rental").value
+    nbSeprate_rental = document.getElementById("nbseparate_rental").value
     nbFloorcorp = document.getElementById("nbfloor").value
     nbSsolcorp = document.getElementById("nbssol").value
     nbParkingcrop = document.getElementById("nbparking").value
     nbMaxfloorcorp = document.getElementById("maxfloor").value
-    console.log(nbFloorcorp)
+    console.log(nbSeprate_rental)
 }
 
 
 // Calculation Hydrid*/
 function hybrid() {
     console.log("test")
-    nbCommerceh = document.getElementById("nbcommerce").value
+    nbCommerce = document.getElementById("nbcommerce").value
     nbFloorh = document.getElementById("nbfloor").value
     nbSsolh = document.getElementById("nbssol").value
     nbParkingh = document.getElementById("nbparking").value
-    nbMaxfloorh = document.getElementById("nbmaxfloor").value
-    nbnbHoursh = document.getElementById("nbHours").value
-    console.log(nbSsol)
+    nbMaxfloor = document.getElementById("nbmaxfloor").value
+    nbHoursh = document.getElementById("nbhours").value
+    console.log(nbHoursh)
 }
+
+// radio bonton recuparation/
+
+var valeur;
+console.log("test")
+if (document.getElementById("standard").checked) {
+ valeur = document.getElementById("standard").value;
+}
+var valeur;
+if (document.getElementById("premium").checked) {
+ valeur = document.getElementById("premium").value;
+}
+var valeur;
+if (document.getElementById("excelium").checked) {
+ valeur = document.getElementById("excelium").value;
+}
+
+
+    var priceStandard = 7565.00 + "$";
+	var priceInstStandard = 1.1;
+	var pricePremium = 12345.00 + "$";
+	var priceInstPremium = 1.13;
+	var priceExcelium = 15400.00 + "$";
+	var priceInstExcelium = 1.16;
